c3b273cd3211c2fdf6066c8ca738dcb45a98f2d5
lwoites <lautaro.woites@gmail.com>
2019-02-18 22:39:24 -0300

    add user and company to review serializer

--- django/consumeraffairs/reviews/serializers.py
+++ django/consumeraffairs/reviews/serializers.py
@@ -10,21 +10,23 @@ class CompanySerializer(serializers.ModelSerializer):
 
 
 class ReviewSerializer(serializers.ModelSerializer):
-    company = CompanySerializer(read_only=True)
+    company = serializers.PrimaryKeyRelatedField(queryset=Company.objects.all())
+    reviewer = serializers.PrimaryKeyRelatedField(read_only=True)
 
     class Meta:
         model = Review
-        fields = ('id', 'title', 'summary', 'rating', 'created', 'reviewer_ip', 'company')
-        read_only_fields = ('reviewer_ip',' company')
+        fields = ('id', 'title', 'summary', 'rating', 'created', 'reviewer_ip', 'company', 'reviewer')
+        read_only_fields = ('reviewer_ip',)
 
 
     def create(self, validated_data):
         request = self.context.get('request')
+        user = request.user
         user_ip = request.META.get('REMOTE_ADDR')
 
         review = Review(**validated_data)
         review.reviewer_ip = user_ip
+        review.reviewer = user
         review.save()
 
         return review
-
